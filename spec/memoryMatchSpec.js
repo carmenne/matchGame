ff55de74a754b51bfd665763fae1f13928aa65f7
carmenne <=>
2018-01-13 17:11:48 +0100

    Added tests

---
+++ spec/memoryMatchSpec.js
@@ -0,0 +1,36 @@
+var game = require("../app/memoryMatch.js");
+
+describe("Memory Match game suite", function() {
+
+  describe("Generate random numbers", function() {
+
+    it("Should generate a random integer between 1-9", function() {
+      for (var i = 0; i < 100; i++) {
+        var actual = game.getRandomInteger(1,10);
+        expect(actual).toBeLessThan(10)
+        expect(actual).toBeGreaterThan(0);
+      }
+    });
+
+    it("Should generate different integers", function() {
+      for (var i = 0; i < 100; i++) {
+        var items = game.createAnswers();
+        var uniqueItems = [...new Set(items)]
+        expect(items.length).toBe(4);
+      }
+    });
+
+    it("Should shuffle an array", function() {
+      var input = [1,1,2,2,3,3,4,4];
+      var actual = game.shuffle(input);
+      expect(actual).not.toBe(input);
+    });
+
+    it("Should generate a shuffled array of 8 integers " +
+      "which is the double of an array of 4 integers]", function() {
+      var actual = game.createFullAnswer();
+      var actualSorted = actual.slice().sort();
+      expect(actual).not.toBe(actualSorted);
+    });
+  });
+});
